adde81e1cac5349ed4f40a3fc9fef5a4e70cf6e6
Fals3Andro1d <davilasturias89@gmail.com>
2018-04-09 18:45:31 -0700

    Added a review endpoint

--- app.js
+++ app.js
@@ -29,4 +29,20 @@ app.get('/api/restaurant', (req, res, next) => {
     });
 });
 
+app.get('/api/reviews', (req, res, next) => {
+  const params = {
+    businessID: req.query.businessID
+  };
+
+  YelpService.review(params)
+    .then((response) => {
+      console.log(response);
+      res.send({ review: response });
+    })
+    .catch((error) => {
+      console.error(error.toString());
+      res.status(503).send({message: error.toString()});
+    });
+});
+
 app.listen(port, () => console.log(`Listening on port ${port}`));
