82e4964155c9b55320f8182fc4bbc92208754c1a
Kelsey Langlois <kels.langlois@gmail.com>
2018-04-16 19:43:31 -0700

    Move GET review endpoint to serverless handler.

--- handler.js
+++ handler.js
@@ -36,3 +36,33 @@ module.exports.restaurant = (event, context, callback) => {
   // Use this code if you don't use the http event with the LAMBDA-PROXY integration
   // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
 };
+
+module.exports.review = (event, context, callback) => {
+  const params = {
+    businessID: event.queryStringParameters.businessID
+  };
+
+  YelpService.review(params)
+    .then((result) => {
+      console.log(result);
+      const response = {
+        statusCode: 200,
+        body: JSON.stringify({
+          review: result
+        }),
+      };
+      callback(null, response);
+    })
+    .catch((error) => {
+      console.error(error.toString());
+      const response = {
+        statusCode: 503,
+        body: JSON.stringify({
+          message: error.toString()
+        }),
+      };
+      callback(null, response);
+    });
+  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
+  // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
+};
